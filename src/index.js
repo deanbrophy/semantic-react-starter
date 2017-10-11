0bb3ea67c30e90f4f99462497393aca5d398bf38
Dean Brophy <dbrophy@ihsmarkit.com>
2017-10-10 19:07:46 -0600

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+import './index.css';
+// import registerServiceWorker from './registerServiceWorker';
+
+const app = React.createElement(App);
+ReactDOM.render(app, document.getElementById('root'));
+// registerServiceWorker();
